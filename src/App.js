3c32cd34359d81cc391562e18c3e95fdae180dcc
sshrik <chj878194@naver.com>
2021-05-06 21:45:13 +0900

    SquashButton Ratio Finish.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SquashButton from './component/SquashButton';
 function App() {
     return(
         <div style={{width: 1000, height: 500, backgroundColor: "#f7f6f3", display: 'flex', justifyContent: "center", alignItems: "center"}}>
-            <SquashButton startY={-400} style={{width: 500, height: 50}} />
+            <SquashButton startY={-400} style={{width: 500, height: 50, backgroundColor: "#1e1e1e"}} />
         </div>
     )
 }
--- src/component/SquashButton.js
+++ src/component/SquashButton.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
-const squashDown = (width, height, top) => {
-    var toWidth = parseInt(width * 1.4);
-    var toWidth2 = parseInt(width * 0.6);
-    var toHeight = parseInt(height * 0.6);
-    var toHeight2 = parseInt(height * 1.4);
-    var toTop = parseInt(top * -1 * 0.1);
-    var toTop2 = parseInt(height * 0.2);
-    var toTop3 = parseInt(height * 0.2);
+const squashDown = (width, height, top, squashRatio=0.3, movingRatio=0.15) => {
+    var bigSRatio = 1 + squashRatio;
+    var smallSRatio = 1 - squashRatio;
+    var toWidth = parseInt(width * bigSRatio);
+    var toWidth2 = parseInt(width * smallSRatio);
+    var toHeight = parseInt(height * smallSRatio);
+    var toHeight2 = parseInt(height * bigSRatio);
+    var toTop = parseInt(top * -1 * movingRatio);
+    var toTop2 = parseInt(top * movingRatio);
+    var toTop3 = parseInt(height * squashRatio);
 
     return (keyframes`
         0% {
             margin-top: ${top}px;
         }
-        85%{
+        74%{
             margin-top: ${toTop}px;
             width: ${width}px;
             height: ${height}px;
         }
-        90% {
+        84% {
             marginTop: ${toTop2}px;
             width: ${toWidth}px;
             height: ${toHeight}px;
         }
-        95% {
+        94% {
             margin-top: ${toTop3}px;
             width: ${toWidth2}px;
             height: ${toHeight2}px;
         }
         100% {
+            margin-top: 0px;
             width: ${width}px;
             height: ${height}px;
-            margin-top: 0px;
         }
     `);
 }
@@ -41,11 +43,9 @@ const ButtonContainer = styled.div`
     display: flex;
     justifyContent: center;
     alignItems: center;
-    background-color: #ffffff;
 `;
 
 const SButton = styled.button`
-    background-color: #341434;
     width: ${props => {
         if(props.style) return props.style.width+"px" || "300px";
         else return "300px";
@@ -54,7 +54,6 @@ const SButton = styled.button`
         if(props.style) return props.style.height+"px" || "30px";
         else return "30px";
     }};
-    border-radius: 0px;
 
     animation: ${props => {
         var width, height;
@@ -68,17 +67,8 @@ const SButton = styled.button`
         }
         var top = props.startY;
         return squashDown(width, height, top);
-    }} 1.5s 0s 1 ease-in;
-
-    &:hover {
-        background: #803180;
-    }
-    &:active {
-        background: #341434;
-    }
+    }} 1s 0s 1 ease-in;
 `;
-
-
 
 function SquashButton(props) {
     return(
